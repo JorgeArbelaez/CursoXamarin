83c8210f1d50aa0540bff948ba6deef4a99f3e9a
Jorge Arbelaez <jearbelaezr@audisoft.com>
2020-01-27 18:18:29 -0500

    Aplicacion multi pagina con navegacion jerarquica

---
+++ Paginas/Paginas/Paginas/MainPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Paginas
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void ButtonPage1_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new Page1());
+        }
+
+        private async void ButtonPage2_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new Page2());
+        }
+    }
+}
