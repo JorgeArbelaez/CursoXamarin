b0ffc2b6839f8a9fd71d8ab0732b7917a4db6bde
Jorge Arbelaez <jearbelaezr@audisoft.com>
2020-01-27 18:16:45 -0500

    Funciones DisplayAlert y DisplayActionSheet

--- MostrarioComponentes/MostrarioComponentes/MostrarioComponentes/MainPage.xaml.cs
+++ MostrarioComponentes/MostrarioComponentes/MostrarioComponentes/MainPage.xaml.cs
@@ -23,5 +23,25 @@ namespace MostrarioComponentes
             placeHolderDireccion.Text = switchActivar.IsToggled.ToString();
             //placeHolderDireccion.Text = ((Switch)sender).IsToggled.ToString();
         }
+
+        private async void MostrarDireccion_Clicked(object sender, EventArgs e)
+        {
+            await DisplayAlert("Mensaje", $"Dirección: {placeHolderDireccion.Text}", "Ok");
+        }
+
+        private async void ButtonPregunta_Clicked(object sender, EventArgs e)
+        {
+            bool activar = await DisplayAlert("Activar", "¿Desea activar?", "Sí", "No");
+            switchActivar.IsToggled = activar;
+        }
+
+        private async void ButtonCiudad_Clicked(object sender, EventArgs e)
+        {
+            string ciudad = await DisplayActionSheet("Ciudad", "Cancelar", null, "Bogotá", "Cali", "Medellín");
+            if (ciudad != "Cancelar")
+            {
+                EditorComentarios.Text += "\n" + ciudad;
+            }
+        }
     }
 }
